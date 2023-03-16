3360fd981062ee8ac1120ded08f7c40132be7ff7
mortred <94474419+traxes99@users.noreply.github.com>
2023-03-16 21:33:38 +0100

    Add files via upload

---
+++ components/connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_name = 'mysql:host=localhost;dbname=food_db';
+$user_name = 'mortred';
+$user_password = 'coupdeGrace!00';
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+?>
---
+++ components/footer.php
@@ -0,0 +1,47 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:test@gmail.com">test@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>00:07am to 00:10pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Kruševo</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:045">045</a>
+         <a href="tel:044">044</a>
+      </div>
+    
+      <div class="box">
+         <img src="images/instagram.png" alt="">
+         <h3>social</h3>
+         <a href="tel:045">username</a>
+      </div>
+
+      
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>mortred</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
+
